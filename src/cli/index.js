75a4fd41a75ec98bc579fc540e64b81e1a93b824
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-07 13:59:33 +0530

    Setup CLI command with name, version, and description

--- src/cli/index.js
+++ src/cli/index.js
@@ -2,9 +2,16 @@
 
 //import local modules
 import { generateDocumentation, initializeConfig } from './commands/index.js';
+import pkg from '../../package.json' with { type: 'json' };
 
 // import external modules
 import { program } from 'commander';
+
+// setup CLI command
+program
+  .name('docs-gen-openapi')
+  .version(pkg.version, '-v, --version', 'Output the current version of docs-gen-openapi')
+  .description('A CLI tool to generate documentation from OpenAPI specifications');
 
 // init CLI command
 program
