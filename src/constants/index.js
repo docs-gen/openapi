532e9ad166aa67a9aaf2f6985d0004805a01539e
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-07 19:32:42 +0530

    Add OPEN_API_CONFIG constant and remove unused internal index file

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const OPEN_API_CONFIG = {
+  FILE_NAME: 'docsgen.config.js',
+  SUPPORTED_OPEN_API_VERSIONS: [
+    {
+      name: 'OpenAPI 3.0.x',
+      value: '3.0.x',
+    },
+    {
+      name: 'OpenAPI 3.1.x',
+      value: '3.1.x',
+    },
+    {
+      name: 'OpenAPI 3.2.x',
+      value: '3.2.x',
+    },
+  ],
+  TYPE_DEFS_MAP: {
+    '3.0.x': 'DocsGeneratorOptionsV30X',
+    '3.1.x': 'DocsGeneratorOptionsV31X',
+    '3.2.x': 'DocsGeneratorOptionsV32X',
+  },
+};
