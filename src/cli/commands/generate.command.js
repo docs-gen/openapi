0cf8dc80775649bf4e7731eae9410e9ea998794c
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-09 22:38:02 +0530

    Implement generate function to validate config file and update generateDocsByCLI to use configFilePath

--- src/cli/commands/generate.command.js
+++ src/cli/commands/generate.command.js
@@ -1,5 +1,11 @@
+// import local modules
 import { generate } from '../../lib/generate.js';
 
+// function to invoke the generation of docs via CLI
 export async function generateDocsByCLI() {
-  await generate();
+  const projectRoot = process.cwd();
+  const configFilePath = path.join(projectRoot, OPEN_API_CONFIG.FILE_NAME);
+
+  // invoke generation
+  await generate({ configFilePath });
 }
--- src/lib/generate.js
+++ src/lib/generate.js
@@ -1,1 +1,8 @@
-export async function generate() {}
+// import local modules
+import { validate } from './validate.js';
+
+// function to generate the docs
+export async function generate({ configFilePath }) {
+  // validate the config file
+  await validate({ configFilePath });
+}
