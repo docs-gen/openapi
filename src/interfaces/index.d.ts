82260aca249d48906ba5a2adc734ccceb0a28894
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-10 02:57:50 +0530

    Add OpenAPI 3.0.0, 3.1.0, and 3.2.0 interface definitions and update index exports

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,3 +1,3 @@
-export { DocsGeneratorOptions as DocsGeneratorOptionsV30X } from './3.0.x.gen';
-export { DocsGeneratorOptions as DocsGeneratorOptionsV31X } from './3.1.x.gen';
-export { DocsGeneratorOptions as DocsGeneratorOptionsV32X } from './3.2.x.gen';
+export { DocsGeneratorOptions as DocsGeneratorOptionsV30X } from './3.0.0.gen';
+export { DocsGeneratorOptions as DocsGeneratorOptionsV31X } from './3.1.0.gen';
+export { DocsGeneratorOptions as DocsGeneratorOptionsV32X } from './3.2.0.gen';
