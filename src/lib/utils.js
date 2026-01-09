e597aa38270e1bad90ecb76e54778b04e55b4388
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-10 02:23:21 +0530

    Refactor utils.js: reorganize imports and enhance readSchemaFile function for better clarity

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,10 @@
+// import local modules
+import { OPEN_API_CONFIG } from '../constants/index.js';
+
 // import external modules
 import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
 
 // function to check if a config file already exists
 export async function configFileExists(filePath) {
@@ -7,4 +12,17 @@ export async function configFileExists(filePath) {
     .stat(filePath)
     .then(() => true)
     .catch(() => false);
+}
+
+// function to resolve schema file path and read its content
+export async function readSchemaFile({ openAPIVersion }) {
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const schemaDir = path.join(__dirname, '../schemas');
+  const schemaFilePath = path.resolve(
+    schemaDir,
+    `${OPEN_API_CONFIG.SCHEMA_FILES_MAP[openAPIVersion]}`
+  );
+
+  return await fs.readFile(schemaFilePath, 'utf-8');
 }
