31ab97ea380f0491073cda959f9072ba5cb4cfad
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-10 02:58:18 +0530

    Fix OpenAPI version values and update schema file mappings in OPEN_API_CONFIG

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,25 +3,25 @@ export const OPEN_API_CONFIG = {
   SUPPORTED_OPEN_API_VERSIONS: [
     {
       name: 'OpenAPI 3.0.x',
-      value: '3.0.x',
+      value: '3.0.0',
     },
     {
       name: 'OpenAPI 3.1.x',
-      value: '3.1.x',
+      value: '3.1.0',
     },
     {
       name: 'OpenAPI 3.2.x',
-      value: '3.2.x',
+      value: '3.2.0',
     },
   ],
   SCHEMA_FILES_MAP: {
-    '3.0.x': '3.0.x.gen.schema.json',
-    '3.1.x': '3.1.x.gen.schema.json',
-    '3.2.x': '3.2.x.gen.schema.json',
+    '3.0.0': '3.0.0.gen.schema.json',
+    '3.1.0': '3.1.0.gen.schema.json',
+    '3.2.0': '3.2.0.gen.schema.json',
   },
   TYPE_DEFS_MAP: {
-    '3.0.x': 'DocsGeneratorOptionsV30X',
-    '3.1.x': 'DocsGeneratorOptionsV31X',
-    '3.2.x': 'DocsGeneratorOptionsV32X',
+    '3.0.0': 'DocsGeneratorOptionsV30X',
+    '3.1.0': 'DocsGeneratorOptionsV31X',
+    '3.2.0': 'DocsGeneratorOptionsV32X',
   },
 };
