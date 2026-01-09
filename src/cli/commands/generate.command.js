7aab40c996a47982911406383846e19d76e98156
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-10 03:30:13 +0530

    Refactor generate function: improve error handling and load config dynamically

--- src/cli/commands/generate.command.js
+++ src/cli/commands/generate.command.js
@@ -1,10 +1,13 @@
 // import local modules
+import { OPEN_API_CONFIG } from '../../constants/index.js';
 import { generate } from '../../lib/generate.js';
+
+// import external modules
+import path from 'path';
 
 // function to invoke the generation of docs via CLI
 export async function generateDocsByCLI() {
-  const projectRoot = process.cwd();
-  const configFilePath = path.join(projectRoot, OPEN_API_CONFIG.FILE_NAME);
+  const configFilePath = path.join(process.cwd(), OPEN_API_CONFIG.FILE_NAME);
 
   // invoke generation
   await generate({ configFilePath });
--- src/lib/generate.js
+++ src/lib/generate.js
@@ -1,8 +1,35 @@
 // import local modules
+import { configFileExists } from './utils.js';
 import { validate } from './validate.js';
+
+// import external modules
+import url from 'url';
+import kleur from 'kleur';
+
+// sub-function to load config
+async function loadConfig(configFilePath) {
+  const configModule = await import(url.pathToFileURL(configFilePath).href);
+  return configModule.default;
+}
 
 // function to generate the docs
 export async function generate({ configFilePath }) {
-  // validate the config file
-  await validate({ configFilePath });
+  try {
+    // check if config file exists
+    if (!(await configFileExists(configFilePath)))
+      throw new Error(`Config file not found at ${configFilePath}`);
+
+    // load config file
+    const configModule = await loadConfig(configFilePath);
+    if (!configModule) throw new Error('Failed to load config from the file');
+
+    // validate the config module
+    await validate({ configModule });
+  } catch (error) {
+    console.error(
+      `${kleur.red('✖')} ${kleur.bold().blue('@docs-gen/openapi:')} ${kleur
+        .bold()
+        .grey('Error during docs generation:')} ${kleur.red(error.message)}`
+    );
+  }
 }
