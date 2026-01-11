89fe7c1315c31822b81b2add142d78b72f03953f
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-11 14:52:11 +0530

    Refactor config file generation logic and update schema file handling in initialize function

--- src/lib/initialize.js
+++ src/lib/initialize.js
@@ -1,20 +1,18 @@
 // import internal modules
 import { OPEN_API_CONFIG } from '../utils/constants.js';
-import { readSchemaFile } from '../utils/helpers.js';
+import { getSchemaFilePath, readSchemaFile } from '../utils/helpers.js';
 
 // import external modules
 import fs from 'fs/promises';
 import util from 'util';
 
-// sub-function to generate config file content based on OpenAPI version
-function generateFileContent({ schemaFileContent, openAPIVersion }) {
-  const versionTypeDefs = OPEN_API_CONFIG.TYPE_DEFS_MAP[openAPIVersion];
-
+// sub-function to generate config file contents
+function generateConfigFileContents({ schemaFileContents, typeDefVersion }) {
   return `/**
- * @type {import('@docs-gen/openapi').${versionTypeDefs}}
+ * @type {import('@docs-gen/openapi').${typeDefVersion}}
  */
 
-export default ${util.inspect(JSON.parse(schemaFileContent), {
+export default ${util.inspect(JSON.parse(schemaFileContents), {
     compact: false,
     depth: null,
     maxArrayLength: null,
@@ -25,15 +23,21 @@ export default ${util.inspect(JSON.parse(schemaFileContent), {
 
 // function to initialize a config file
 export async function initialize({ openAPIVersion, configFilePath }) {
-  // get schema file content
-  const schemaFileContent = await readSchemaFile({
+  // get schema file path
+  const schemaFilePath = getSchemaFilePath({
     dirName: OPEN_API_CONFIG.SCHEMA_FILES_MAP.TEMPLATES_DIR,
-    openAPIVersion,
+    fileName: OPEN_API_CONFIG.SCHEMA_FILES_MAP[openAPIVersion],
   });
 
-  // generate config file content
-  const configFileContent = generateFileContent({ schemaFileContent, openAPIVersion });
+  // get schema file contents
+  const schemaFileContents = await readSchemaFile({ schemaFilePath });
+
+  // generate config file contents
+  const configFileContents = generateConfigFileContents({
+    schemaFileContents,
+    typeDefVersion: OPEN_API_CONFIG.TYPE_DEFS_MAP[openAPIVersion],
+  });
 
   // write config file
-  return await fs.writeFile(configFilePath, configFileContent, 'utf-8');
+  return await fs.writeFile(configFilePath, configFileContents, 'utf-8');
 }
