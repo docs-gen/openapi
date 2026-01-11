e6023436ad32e3296cf9011b3dadb7f3f34ee253
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-11 16:56:15 +0530

    Refactor validateSchema function: enhance AJV instance creation with plugins update and schema validation process and return result

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,9 @@
 import fs from 'fs/promises';
 import path from 'path';
 import url from 'url';
-import Ajv from 'ajv';
+import Ajv from 'ajv/dist/2020.js';
+import addFormats from 'ajv-formats';
+import ajvErrors from 'ajv-errors';
 
 // function to check if a config file already exists
 export async function configFileExists(filePath) {
@@ -27,16 +29,21 @@ export async function getSchemaFileContents({ schemaFilePath }) {
 
 // function to validate module against its schema
 export async function validateSchema({ validationModule, moduleSchema, additionalSchemas }) {
-  // create ajv instance
-  const ajv = new Ajv({ allErrors: true, strict: true });
+  // create new ajv instance and add plugins
+  const ajvInstance = new Ajv({ allErrors: true, strict: true });
+  addFormats(ajvInstance);
+  ajvErrors(ajvInstance);
 
-  // add additional schemas to ajv instance if any
+  // add additionalSchemas to ajv instance (if any)
   if (additionalSchemas && additionalSchemas.length > 0)
-    additionalSchemas.forEach(schema => ajv.addSchema(schema));
+    additionalSchemas.forEach(additionalSchema => ajvInstance.addSchema(additionalSchema));
 
-  // compile schema
-  const validate = ajv.compile(moduleSchema);
+  // compile moduleSchema
+  const compiledModuleSchema = ajvInstance.compile(moduleSchema);
 
-  // validate module against schema
-  return validate(validationModule);
+  // validate module against compiledModuleSchema
+  const validationResult = compiledModuleSchema(validationModule);
+
+  // return validation result and errors (if any)
+  return { validationResult, validationErrors: compiledModuleSchema.errors };
 }
