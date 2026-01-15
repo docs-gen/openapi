f6a5c31cb07b3c7df965db30cc5cd50ada4cf71b
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-16 02:41:35 +0530

    Moved type definitions for @docs-gen/openapi 3.x from utils/ to src/

--- src/utils/interfaces/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { DocsGeneratorOptions as DocsGeneratorOptionsV30X } from './3.0.0.gen';
-export { DocsGeneratorOptions as DocsGeneratorOptionsV31X } from './3.1.0.gen';
-export { DocsGeneratorOptions as DocsGeneratorOptionsV32X } from './3.2.0.gen';
